ac601522827a80d1aebf881758ed9dd8533f4c90
TaherunNasaToma <taheruntoma@gmail.com>
2025-07-16 03:06:26 +0600

    added post api for category

--- index.js
+++ index.js
@@ -3,16 +3,79 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const app = express();
 
-// Load env vars from .env
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
 dotenv.config();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i41acjo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+
+        //category
+        const db = client.db('medicineDB')
+        const categoryCollection = db.collection('categories')
+
+        app.get('/categories', async (req, res) => {
+            const categories = await categoryCollection.find().toArray();
+            res.send(categories)
+        })
+
+
+        // Add Category (POST)
+        app.post('/categories', async (req, res) => {
+            try {
+                const category = req.body;
+                const result = await categoryCollection.insertOne(category);
+
+                res.status(201).send({
+                    message: "Category added successfully",
+                    insertedId: result.insertedId,
+                });
+            } catch (error) {
+                console.error("Error inserting category:", error);
+                res.status(500).send({ error: 'Internal Server Error' });
+            }
+        });
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 // Sample Routes
 app.get('/', (req, res) => {
-  res.send('Lifenix Medicine Multi-Vendor Server Running...');
+    res.send('Lifenix Medicine Multi-Vendor Server Running...');
 });
+
+app.listen(port, () => {
+    console.log(`Server in listening on port ${port}`)
+})
